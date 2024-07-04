43f27970871bcafcb46e73904c2b0add06a194bc
Denis <denisbormusov@gmail.com>
2024-07-04 22:05:55 +0500

    module_4lesson_09

--- js/cart.js
+++ js/cart.js
@@ -2,31 +2,30 @@
 
 const cart = {
   items: [],
-  totalPrice: 0,
   count: 0,
 
-  getTotalPrice() {
-    return this.totalPrice
-  },
 
   increaseCount(number) {
     this.count = this.count + number;
   },
 
   calculateItemPrice() {
-    this.totalPrice = this.items.reduce((acc, price) => acc + price.itemPrice, 0)
+    return this.items.reduce((acc, price) => acc + price.itemPrice, 0)
   },
+
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
+
+  set totalPrice(value) {},
 
   add(itemName, itemPrice, itemCount = 1) {
     this.items.push({itemName,itemPrice,itemCount});
     this.increaseCount(itemCount);
-    this.calculateItemPrice();
-    this.getTotalPrice();
   },
 
   clear() {
     this.items = [];
-    this.totalPrice = 0;
     this.count = 0;
   },
 
@@ -40,5 +39,6 @@ const cart = {
 cart.add('Ручка', 15);
 cart.add('Карандаш', 20, 2);
 cart.add('Стиралка', 30, 5);
+
 
 cart.print();
